a059492621f9456a1899807c3ff6f7a9c8d750b8
Azib Norazman <azib.naufal@gmail.com>
2021-06-22 10:42:49 +0100

    setup.py and README updated

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
+
+
 
 setup(
         name = 'light_curve_analysis',
         ext_modules = cythonize("analysis_tools_cython.pyx"),
+        include_dirs=[numpy.get_include()]
 )
 
