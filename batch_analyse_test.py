543eae3be0fd1e2f5519180e112ec7ea801768a4
azibn <azib.naufal@gmail.com>
2022-03-03 16:34:37 +0000

    updated version of batch_analyse.py, with changes to code structure, placement and arguments

---
+++ batch_analyse_test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# First have to disable inbuilt multithreading for performance reasons.
+import os
+#import pandas as pd
+os.nice(8)
+os.environ['OMP_NUM_THREADS']='1'
+from analysis_tools_cython import *
+import multiprocessing
+import sys
+import traceback
+import argparse
+import tqdm
+import data
+import glob
+import loaders
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+parser = argparse.ArgumentParser(description='Analyse lightcurves in target directory.')
+parser.add_argument(help='target directory(s)',
+                        default='.',nargs='+',dest='path')
+
+parser.add_argument('-t', help='number of threads to use',default=1,
+                        dest='threads',type=int)
+
+parser.add_argument('-o',default=f'output.txt',dest='of',help='output file')
+parser.add_argument('-q', help='Keep only points with SAP_QUALITY=0',action='store_true')
+parser.add_argument('-f',help='select flux. "PDCSAP_FLUX is default. XRP lightcurve options are "corrected flux", "PCA flux" or "raw flux"',dest='f',default='PDCSAP_FLUX')
+parser.add_argument('-c',help='select sigma clipping threshold for XRP lightcurves.', dest='c',default=4,type=int)
+
+# Get directories from command line arguments.
+args = parser.parse_args()
+
+paths = []
+for path in args.path:
+    paths.append( os.path.expanduser(path) )
+
+## Prepare multithreading.
+multiprocessing.set_start_method("fork") # default for >=3.8 is spawn
+m = multiprocessing.Manager()
+lock = m.Lock()
+
+
+def mission_lightcurves(f_path):
+    try:
+        f = os.path.basename(f_path)
+        print(f)
+        table = import_lightcurve(f_path, flux=args.f,drop_bad_points=args.q)
+        processing(table,f_path)
+        lock.acquire()
+        with open(args.of,'a') as out_file:
+            out_file.write(result_str+'\n')
+        lock.release()
+    except (KeyboardInterrupt, SystemExit):
+        print("Process terminated early, exiting",file=sys.stderr)
+        raise
+    except Exception as e:
+        print("\nError with file "+f_path,file=sys.stderr)
+        traceback.print_exc()
+    
+
+def xrp_lightcurves(f_path):
+    try:
+        f = os.path.basename(f_path)
+        print(f_path)
+        table = import_XRPlightcurve(f_path,sector=sector_test,clip=args.c)[0]
+        table = table["time", args.f, "quality"]
+        result_str = processing(table,f_path)
+        lock.acquire()
+        with open(args.of,'a') as out_file:
+            out_file.write(result_str+'\n')
+        lock.release()
+    except (KeyboardInterrupt, SystemExit):
+        print("Process terminated early, exiting",file=sys.stderr)
+        raise
+    except Exception as e:
+        print("\nError with file "+f_path,file=sys.stderr)
+        traceback.print_exc()
+
+if __name__ == '__main__':
+    sector_test = args.path[0].split('_')[-2] # this is the case for XRP lightcurves... This is not required for mission lightcurves so it is ok to not consider them.
+    pool = multiprocessing.Pool(processes=args.threads)
+
+    for path in paths:
+        if not os.path.isdir(path):
+            print(path,'not a directory, skipping.',file=sys.stderr)
+            continue
+        
+        # if we are in the lowest subdirectory, perform glob this way.
+        if not list(folders_in(path)):
+            print("this is the lowest subdirectory")
+
+            # this should work for both Kepler and TESS fits files.
+            fits_files = glob.glob(os.path.join(path,'*lc.fits'))
+            pkl_files = glob.glob(os.path.join(path,'*.pkl'))
+            
+            pool.map(mission_lightcurves, fits_files)
+            pool.map(xrp_lightcurves,pkl_files)
+
+        else:
+            print("globbing subdirectories")
+
+            # Start at Sector directory, glob goes through `target/000x/000x/xxxx/**/*lc.fits`
+            fits_files = glob.glob(os.path.join(path,'target/**/**/**/**/*lc.fits')) 
+
+            # These are test SPOC files that I have in my home CSC directory
+            test_fits = glob.glob(os.path.join(path,'**/**/*lc.fits'))
+
+            # Starts at sector directory. globs files in one subdirectory level below
+            pkl_files = glob.glob(os.path.join(path,'**/*.pkl'))
+
+            pool.map(xrp_lightcurves,pkl_files)
+            pool.map(mission_lightcurves, fits_files)
+        
