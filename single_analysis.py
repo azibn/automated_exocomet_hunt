dd4768882c1efd48a06a4473bb18ec8e1680c5b5
azibn <azib.naufal@gmail.com>
2022-05-03 11:38:50 +0100

    plotting corrections

--- single_analysis.py
+++ single_analysis.py
@@ -31,26 +31,27 @@ args = parser.parse_args()
 # If XRP TESS lightcurve, apply MAD. If Kepler lightcurve, skip to timestep
 if (os.path.split(args.fits_file[0])[1].startswith("kplr")) or (
     os.path.split(args.fits_file[0])[1].startswith("hlsp_tess")
-    and os.path.split(args.fits_file[0])[1].endswith("fits")):
-    #or os.path.split(args.fits_file[0])[1].startswith("tess")
-    #and os.path.split(args.fits_file[0])[1].endswith("fits")
-#):
+    and os.path.split(args.fits_file[0])[1].endswith("fits")
+):
+    # or os.path.split(args.fits_file[0])[1].startswith("tess")
+    # and os.path.split(args.fits_file[0])[1].endswith("fits")
+    # ):
     table = import_lightcurve(args.fits_file[0])
     t, flux, quality, real = clean_data(table)
 
 
-elif 'tasoc' in os.path.split(args.fits_file[0])[1]:
+elif "tasoc" in os.path.split(args.fits_file[0])[1]:
     table = import_tasoclightcurve(args.fits_file[0])
-    plt.plot(table['TIME'],table['FLUX_CORR'])
+    plt.plot(table["TIME"], table["FLUX_CORR"])
     t, flux, quality, real = clean_data(table)
 
 
 elif os.path.split(args.fits_file[0])[1].endswith(".csv"):
-    table = import_eleanor(args.fits_file[0],6,1,4,drop_bad_points=True)
+    table = import_eleanor(args.fits_file[0], 6, 1, 4, drop_bad_points=True)
     t, flux, quality, real = clean_data(table)
 else:
-    table, lc_info = (
-        import_XRPlightcurve(args.fits_file[0], sector=6, clip=4, drop_bad_points=True)
+    table, lc_info = import_XRPlightcurve(
+        args.fits_file[0], sector=6, clip=4, drop_bad_points=True
     )
     to_clean = table["time", "PCA flux", "quality"]
     t, flux, quality, real = clean_data(to_clean)
@@ -74,14 +75,14 @@ A_mag = np.abs(np.fft.rfft(flux))
 
 flux_ls = np.copy(flux)
 
-#freq, powers = lombscargle_plotting(t,flux_ls,real,0.1)
+# freq, powers = lombscargle_plotting(t,flux_ls,real,0.1)
 lombscargle_filter(t, flux_ls, real, 0.08)  # happens in-place. 0.05 is minimum score
 
-periodicnoise_ls = flux - flux_ls 
+periodicnoise_ls = flux - flux_ls
 flux_ls = flux_ls * real
 
 
-freq, powers = lombscargle_plotting(t,flux_ls,real,0.08)
+freq, powers = lombscargle_plotting(t, flux_ls, real, 0.08)
 # T1 = test_statistic_array(filteredflux, 60)
 T = test_statistic_array(flux_ls, 60 * factor)
 data_nonzeroT = nonzero(T)
@@ -91,35 +92,24 @@ m, n = np.unravel_index(
     T.argmin(), T.shape
 )  # T.argmin(): location of  T.shape: 2D array with x,y points in that dimension
 
-minT = T[m, n]
-minT_time = t[n]
-minT_flux = flux[n]
-minT_duration = m * timestep
+# minT = T[m, n]
+# minT_time = t[n]
+# minT_flux = flux[n]
+# minT_duration = m * timestep
 
-masked_flux = np.copy(flux) 
-masked_flux[n-72:n+72] = 0 # placeholder. need to chnage to match duration 
-#plt.plot(t,masked_flux)
-test = np.copy(masked_flux)
+masked_flux = np.copy(flux)
+masked_flux[n - 72 : n + 72] = 0  # placeholder. need to chnage to match duration
+
+original_masked_flux = np.copy(masked_flux)
 lombscargle_filter(t, masked_flux, real, 0.08)
-#plt.plot(t,masked_flux-0.001)
-#plt.plot(t,(test - masked_flux)-0.002)
-periodicnoise_ls2 = test - masked_flux
-#plt.figure(figsize=(20,15))
-#plt.plot(t,(flux - periodicnoise_ls2))
-#plt.plot(t,flux-0.001)
-
-
+periodicnoise_ls2 = original_masked_flux - masked_flux
 masked_flux = masked_flux * real
 final_flux = flux - periodicnoise_ls2
-#plt.plot(t,final_flux-0.001)
-#plt.plot(t,periodicnoise_ls)
 
 T_new = test_statistic_array(final_flux, 60 * factor)
 data_nonzeroT = nonzero(T_new)
 
-m2, n2 = np.unravel_index(
-    T_new.argmin(), T_new.shape
-)
+m2, n2 = np.unravel_index(T_new.argmin(), T_new.shape)
 
 minT = T_new[m2, n2]
 minT_time = t[n2]
@@ -133,20 +123,20 @@ print("   Duration =", round(minT_duration, 2), "days.")
 print("   T =", round(minT, 1))
 print("   T/sigma =", round(minT / data_nonzeroT.std(), 1))
 
-trans_start = n - math.floor((m - 1) / 2)
-trans_end = trans_start + m
+trans_start = n2 - math.floor((m2 - 1) / 2)
+trans_end = trans_start + m2
 print("Transit depth =", round(flux[trans_start:trans_end].mean(), 6))
 
 # Transit shape calculation
 if (
-    n - 3 * m >= 0 and n + 3 * m < N
+    n2 - 3 * m2 >= 0 and n2 + 3 * m2 < N
 ):  # m: width of point(s) in lc. first part: 3 transit widths away from first data point. last part: not more than 3 transit widths away.
-    t2 = t[n - 3 * m : n + 3 * m]
-    x2 = final_flux[n - 3 * m : n + 3 * m]
+    t2 = t[n2 - 3 * m2 : n2 + 3 * m2]
+    x2 = final_flux[n2 - 3 * m2 : n2 + 3 * m2]
     q2 = quality[
-        n - 3 * m : n + 3 * m
+        n2 - 3 * m2 : n2 + 3 * m2
     ]  # quality points from three transit widths to other edge of three transit widths.
-    background = (sum(x2[: 1 * m]) + sum(x2[5 * m :])) / (2 * m)
+    background = (sum(x2[: 1 * m2]) + sum(x2[5 * m2 :])) / (2 * m2)
     x2 -= background
     paramsgauss = single_gaussian_curve_fit(t2, -x2)
     y2 = -gauss(t2, *paramsgauss)
@@ -162,32 +152,32 @@ if (
     print("Quality flags:", qual_flags)
 
 # Classify events
-asym, _, _ = calc_shape(m, n, t, flux)
-print(classify(m, n, real, asym))
+asym, _, _ = calc_shape(m2, n2, t, flux)
+print(classify(m2, n2, real, asym))
 
 # Skip plotting if no graphical output set
 if args.n:
     sys.exit()
 
-fig1, axarr = plt.subplots(8,figsize=(13,22))
+fig1, axarr = plt.subplots(8, figsize=(13, 22))
 # plt.rcParams['font.size'] = '16'
 # plt.rcParams['axes.labelsize'] = '16'
 axarr[0].plot(A_mag)  # fourier plot
 axarr[0].title.set_text("Fourier plot")
-axarr[0].set_xlabel('frequency')
-axarr[0].set_ylabel('power')
-axarr[1].plot(freq,powers)
-axarr[1].title.set_text('Lomb-Scargle plot')
-axarr[1].set_xlabel('frequency')
-axarr[1].set_ylabel('Lomb-Scargle power')
+axarr[0].set_xlabel("frequency")
+axarr[0].set_ylabel("power")
+axarr[1].plot(freq, powers)
+axarr[1].title.set_text("Lomb-Scargle plot")
+axarr[1].set_xlabel("frequency")
+axarr[1].set_ylabel("Lomb-Scargle power")
 axarr[2].plot(t, flux + ones, label="flux")
 axarr[2].plot(t, periodicnoise_ls + ones, label="periodic noise")
 axarr[2].title.set_text("Original lightcurve and the periodic noise")
 axarr[2].set_xlabel("Days in BTJD")
 axarr[2].set_ylabel("Normalised flux")
-axarr[2].legend(loc='lower left')
+axarr[2].legend(loc="lower left")
 axarr[3].plot(t, flux_ls + ones)  # lomb-scargle plot
-#axarr[3].plot(m,marker='o')
+# axarr[3].plot(m,marker='o')
 axarr[3].title.set_text("First noise-removed lightcurve (first Lomb-Scargle)")
 axarr[3].set_xlabel("Days in BTJD")
 axarr[3].set_ylabel("Normalised flux")
@@ -205,14 +195,14 @@ axarr[4].title.set_text("An image of the lightcurve (first Lomb Scargle)")
 axarr[4].set_xlabel("Days in BTJD")
 axarr[4].set_ylabel("Transit width in days")
 axarr[4].set_aspect("auto")
-axarr[5].plot(t,test+ones,label='masked flux')
-axarr[5].plot(t,periodicnoise_ls2+ones,label='periodic noise')
+axarr[5].plot(t, original_masked_flux + ones, label="masked flux")
+axarr[5].plot(t, periodicnoise_ls2 + ones, label="periodic noise")
 axarr[5].legend()
-axarr[5].title.set_text('Lomb-Scargle applied on masked flux')
-axarr[6].plot(t,final_flux+ones,label='final flux')
-axarr[6].plot(t,flux+ones-0.001,color='red',label='original flux')
+axarr[5].title.set_text("Lomb-Scargle applied on masked flux")
+axarr[6].plot(t, final_flux + ones, label="final flux")
+axarr[6].plot(t, flux + ones - 0.001, color="red", label="original flux")
 axarr[6].legend()
-axarr[6].title.set_text('Cleaned flux')
+axarr[6].title.set_text("Cleaned flux")
 
 im = axarr[7].imshow(
     T_new,
@@ -248,7 +238,6 @@ except FileExistsError:
     pass
 
 fig1.savefig("figs_tess/fourier plots", dpi=300)
-
 
 
 # Lightcurve plots
