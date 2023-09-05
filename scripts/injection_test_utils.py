77683cc42f58fdb61e7c13cf4a0de7bda9ced398
azibn <azib.naufal@gmail.com>
2023-09-05 16:52:10 +0100

    added documentation for injection script

--- scripts/injection_test_utils.py
+++ scripts/injection_test_utils.py
@@ -1,7 +1,49 @@
-from analysis_tools_cython import comet_curve
+from analysis_tools_cython import skewed_gaussian
 
 
-def inject_lightcurve(flux, time, depth, injected_time):
+def inject_lightcurve(time, flux, depth, injected_time, sigma, skewness):
+
+    """Injects a simulated transit signal into a given lightcurve.
+
+    This function takes a lightcurve represented by time and flux values and injects
+    a simulated transit signal into it. The transit signal is modeled as a skewed
+    Gaussian profile centered at the `injected_time` with a specified `depth`, `sigma`,
+    and `skewness`.
+
+    Parameters:
+    -----------
+    time : array-like
+        The time values of the original lightcurve data.
+
+    flux : array-like
+        The flux values of the original lightcurve data.
+
+    depth : float
+        The depth of the simulated transit signal, representing the fractional decrease
+        in flux during the transit. Should be between 0 and 1.
+
+    injected_time : float
+        The time at which the center of the simulated transit signal occurs.
+
+    sigma : float
+        The standard deviation of the Gaussian profile, controlling the width of the
+        transit signal.
+
+    skewness : float
+        The skewness of the Gaussian profile, controlling the asymmetry of the transit
+        signal. Positive values skew the transit signal to the right, while negative
+        values skew it to the left.
+
+    Returns:
+    --------
+    injected_flux : array-like
+        The flux values of the lightcurve with the simulated transit signal injected.
+
+    Notes:
+    ------
+    This function assumes that the `skewed_gaussian` function is available and properly
+    defined elsewhere in the codebase."""
+
     return flux * (
-        1 - comet_curve(time, depth, injected_time, 3.02715600e-01, 3.40346173e-01)
+        1 - skewed_gaussian(time, depth, injected_time, sigma, skewness)
     )
